2f67f3967dab57bec75af7e683a21f0d7474a340
MarkusEicher <markuseicher@outlook.com>
2023-04-27 23:13:19 +0200

    Implement Threading

--- _app.py
+++ _app.py
@@ -1,6 +1,10 @@
 import json
 from os import path
+import winsound
+from win10toast import ToastNotifier
+from time import sleep
 
+toaster = ToastNotifier()
 
 # assets
 SUN_VALLEY_THEME = path.join("sunvalley", "sv.tcl")
@@ -13,3 +17,33 @@ JSDTA:dict
 def load_json():
     with open('appdata.json') as jsfile:
         return json.load(jsfile)
+
+def update_json(data: dict):
+    with open('appdata.json', 'w') as jsfile:
+        json.dump(data, jsfile, indent=2)
+
+# notifier
+def _notify(msg, icon=COFFE_ICO, title=None, soundfile=DOLPHIN_WAV):
+    toaster.show_toast(
+        title=title if title else "Notification",
+        msg=msg,
+        icon_path=icon,
+        threaded=True,
+    )
+
+    if soundfile:
+        winsound.PlaySound(soundfile, flags=winsound.SND_FILENAME)
+
+
+def sed_alert():
+    dt = load_json()
+
+    if dt["sedentary_alert"]:
+        interval_secs = 5 #dt["interval"] * 60
+        sleep(interval_secs)
+
+        _notify(
+            msg = "Take a break and hydrate. Do some stretching.",
+        )
+
+        sed_alert()
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from tkinter import Tk, ttk, StringVar, IntVar
 from _app import *
+from threading import Thread
+
+JSDTA = load_json()
 
 App = Tk()
 App.title("Sedentary Alert")
@@ -16,19 +19,56 @@ frame.grid(column=0, row=0, padx=10, pady=10)
 
 
 sedentary_alert = IntVar()
+sedentary_alert.set(
+    1 if JSDTA["sedentary_alert"] else 0
+)
 
 def toggle_sed_alert():
-    print(sedentary_alert.get())
+    JSDTA["sedentary_alert"] = bool(sedentary_alert.get())
+    update_json(JSDTA)
+
+    if JSDTA["sedentary_alert"] and\
+        not SED_TREAD.is_alive():
+        init_sed_thread()
+        SED_TREAD.start()
 
 
 sed_check = ttk.Checkbutton(
-    frame, variable= sedentary_alert,
+    frame, variable=sedentary_alert,
     text = "Sedentary Alert", command=toggle_sed_alert
 )
-sed_check.grid(column=0, row=0, pady=10)
+sed_check.grid(row=0, column=0, columnspan=2, pady=5)
 
+sed_lbl = ttk.Label(frame, text="Interval")
+sed_lbl.grid(row=1, column=0, pady=5)
 
+interval_options = ["15 Min", "20 Min", "30 Min", "45 Min", "60 Min"]
+interval_period = StringVar()
+# interval_period.set(f"{JSDTA['interval']} Min")
 
+def interval_change (interval: str):
+    JSDTA["interval"] = int(interval.split(" ")[0])
+    update_json(JSDTA)
 
+interval_dropdown = ttk.OptionMenu(
+    frame, interval_period, "Select", *interval_options, command=interval_change
+)
+interval_dropdown.grid(row=1, column=1, padx=(10, 0))
+
+interval_period.set(f"{JSDTA['interval']} Min")
+
+# Create a Thread
+
+def init_sed_thread():
+    global SED_TREAD
+    SED_TREAD = Thread(
+        target=sed_alert,
+        daemon=True
+    )
+
+init_sed_thread()
+
+if JSDTA["sedentary_alert"]:
+    SED_TREAD.start()
 
 App.mainloop()
